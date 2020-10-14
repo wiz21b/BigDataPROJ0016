6747b790f6aa9a4dafde6ac18a565dfbd7017a28
Stéphane Champailler <schampailler@skynet.bewq>
2020-10-14 12:39:35 +0200

    added graphics

--- load_data.py
+++ load_data.py
@@ -1,6 +1,7 @@
 import csv
 import urllib.request
 from collections import namedtuple
+import numpy as np
 
 # If you don't have this, run "pip install tabulate"
 
@@ -8,6 +9,7 @@ from tabulate import tabulate
 
 
 observations = []
+rows = []
 
 with urllib.request.urlopen("https://raw.githubusercontent.com/ADelau/proj0016-epidemic-data/main/data.csv") as fp:
 
@@ -17,8 +19,29 @@ with urllib.request.urlopen("https://raw.githubusercontent.com/ADelau/proj0016-e
     head = next(csvreader)
     DataTuple = namedtuple("DataTuple", head)
 
+
     for r in csvreader:
         if r:
             observations.append(DataTuple(*r))
 
+            rows.append( [int(x) for x in r] )
+
 print(tabulate(observations, headers=head))
+
+rows = np.array(rows)
+
+from matplotlib import pyplot as plt
+num_positive=1
+num_hospitalised=2
+num_cumulative_hospitalizations=3
+num_critical=4
+num_fatalities=5
+
+plt.plot( rows[:,num_positive], label='num_positive')
+plt.plot( rows[:,num_hospitalised], label='num_hospitalised')
+plt.plot( rows[:,num_cumulative_hospitalizations], label='num_cumulative_hospitalizations')
+plt.plot( rows[:,num_critical], label='num_critical')
+plt.plot( rows[:,num_fatalities], label='num_fatalities')
+plt.legend()
+plt.xlabel('Days')
+plt.show()
