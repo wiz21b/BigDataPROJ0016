c9db5750bcb87e6ba4872510138d9f77c7b54c95
Stéphane Champailler <schampailler@skynet.bewq>
2020-10-23 18:42:49 +0200

    refactoring

--- utils.py
+++ utils.py
@@ -1,6 +1,64 @@
+from enum import Enum
 import csv
 from collections import namedtuple
 import urllib.request
+
+
+class ObsRow(Enum):
+    # These 5 are the one provided by the teachers.
+    # Do not change them
+
+    DAYS = 0
+    POSITIVE = 1
+    HOSPITALIZED = 2
+    CUMULATIVE_HOSPITALIZATIONS = 3
+    CRITICAL = 4
+    FATALITIES = 5
+
+    # Other data series we work on
+    CUMULATIVE_POSITIVE = 6
+    RECOVERED = 7
+    SUSPECT = 8
+
+    def __str__(self):
+        return self.name.replace("_", " ").lower()
+
+
+class Model:
+    def __init__(self, observations):
+        # """ observations : a numpy array. Each row of the
+        # array corresponds to one type of observation. Each
+        # column correspond to one (daily) observation for the
+        # corresponding type.
+
+        # The rows' numbers must match `ObsRow`.
+        # """
+        raise NotImplementedError
+
+    def fit_parameters(self, error_func):
+        # Call this method to perform a parameters
+        # fit.
+
+        # error_func : the function that will be
+        # used to compute the error between prediction
+        # and actual data.
+        raise NotImplementedError
+
+    def predict(self, days):
+        # Once the parameters have been fit, call this
+        # function to predict values for `days`.
+
+        # This function must return :
+        # - an array of ObsRow describing the result array rows.
+        # - a result array of which the columns are indexed by the
+        #   ObsRow above. The idea is that the model tells what data it
+        #   computes.
+        raise NotImplementedError
+
+
+def residuals_error(results, observations):
+    return results - observations
+
 
 def load_data():
     observations = []
@@ -20,6 +78,8 @@ def load_data():
                 ir = [int(x) for x in r]
                 t = DataTuple(*ir)
                 observations.append(t)
+
+                positive_cumulated += t.num_positive
                 rows.append(ir + [positive_cumulated])
 
     return head, observations, rows
