80879326d940712fc6107744ae1ed5ef98110572
Stéphane Champailler <schampailler@skynet.bewq>
2020-10-29 12:28:16 +0100

    Added GA fitting to Sarah's code, plus moved to cumulative numbers

--- all_models.py
+++ all_models.py
@@ -3,6 +3,8 @@ import numpy as np
 from scipy.integrate import odeint
 from scipy.optimize import minimize as opt_minimize
 from lmfit import minimize, Parameters, report_fit
+from geneticalgorithm import geneticalgorithm as ga
+
 import matplotlib.pyplot as plt
 from utils import ObsRow, Model, residuals_error, load_data
 
@@ -129,7 +131,8 @@ class Sarah1(Model):
 
         self._observations = observations[
             np.ix_(range(nb_observations),
-                   [ObsRow.POSITIVE.value, ObsRow.HOSPITALIZED.value,
+                   [ObsRow.CUMULATIVE_POSITIVE.value,
+                    ObsRow.CUMULATIVE_HOSPITALIZATIONS.value,
                     ObsRow.CRITICAL.value])]
 
         I0 = 1 # self._observations[0][0]
@@ -179,8 +182,8 @@ class Sarah1(Model):
         res = self._predict(self._initial_conditions, days, self._fit_params)
 
         return [ObsRow.SUSPECT,
-                ObsRow.POSITIVE,
-                ObsRow.HOSPITALIZED,
+                ObsRow.CUMULATIVE_POSITIVE,
+                ObsRow.CUMULATIVE_HOSPITALIZATIONS,
                 ObsRow.CRITICAL,
                 ObsRow.RECOVERED], res
 
@@ -327,6 +330,139 @@ class Sarah1(Model):
                           self._observations).ravel()
 
 
+
+class Sarah1GA(Model):
+
+    def __init__(self, observations, N):
+
+        self._N = N
+        nb_observations = observations.shape[0]
+
+        self._observations = observations[
+            np.ix_(range(nb_observations),
+                   [ObsRow.CUMULATIVE_POSITIVE.value,
+                    ObsRow.CUMULATIVE_HOSPITALIZATIONS.value,
+                    ObsRow.CRITICAL.value])]
+
+        I0 = 1 # self._observations[0][0]
+        H0 = self._observations[0][1]
+        C0 = self._observations[0][2]
+        R0 = 0
+        S0 = self._N - I0 - R0 - H0 - C0
+
+        self._initial_conditions = [S0, I0, H0, C0, R0]
+
+        print(self._initial_conditions)
+        self._fit_params = None
+
+    def fit_parameters(self, error_func):
+        gamma1 = (1/10 + 1/5)/2
+        gamma2 = 0.02
+        gamma3 = 0.02
+        beta = 0.14
+
+        num_cumulative_positiv = np.sum(self._observations[:, 0])
+
+        print(f"num_cumulative_positiv {num_cumulative_positiv}")
+        print(self._observations[:, 0])
+
+        tau = 28/num_cumulative_positiv
+
+        delta = 5/18
+
+        params = Parameters()
+        params.add('gamma1', value=gamma1, min=1/10, max=1/5)
+        params.add('gamma2', value=gamma2, min=0, max=5)
+        params.add('gamma3', value=gamma3, min=0, max=5)
+        params.add('beta', value=beta, min=0.01, max=0.5)
+        params.add('tau', value=tau, min=tau*0.8, max=tau*1.2)
+        params.add('delta', value=delta, min=0.6*delta, max=1.4*delta)
+
+        # result = minimize(self._plumb,
+        #                   params,
+        #                   args=(len(self._observations), error_func),
+        #                   method='leastsq')
+
+        # report_fit(result)
+
+        # self._fit_params = result.params
+
+        print(params.items())
+
+        bounds = np.array([(p.min, p.max) for p_name, p in params.items()])
+        self._error_func = error_func
+
+        gamodel = ga(function=self._plumb,
+                     dimension=len(bounds),
+                     variable_type='real',
+                     variable_boundaries=bounds)
+        gamodel.run()
+
+        self._fit_params = self._params_array_to_dict(
+            gamodel.output_dict['variable'])
+
+    def _params_array_to_dict(self, params):
+        return dict(
+            zip(['gamma1', 'gamma2', 'gamma3', 'beta', 'tau', 'delta'],
+                params))
+
+    def predict(self, days):
+        res = self._predict(self._initial_conditions, days, self._fit_params)
+
+        return [ObsRow.SUSPECT,
+                ObsRow.CUMULATIVE_POSITIVE,
+                ObsRow.CUMULATIVE_HOSPITALIZATIONS,
+                ObsRow.CRITICAL,
+                ObsRow.RECOVERED], res
+
+    def _predict(self, initial_conditions, days, params):
+        tspan = np.arange(0, days, 1)
+
+        S0, I0, H0, C0, R0 = initial_conditions
+        gamma1 = params['gamma1']
+        gamma2 = params['gamma2']
+        gamma3 = params['gamma3']
+        beta = params['beta']
+        tau = params['tau']
+        delta = params['delta']
+
+        # Integrate ODE over time span [0,days]
+        res = odeint(self._SIHCR_model, [S0, I0, H0, C0, R0],
+                     tspan, args=(gamma1, gamma2, gamma3, beta, tau, delta))
+        return res
+
+    def _SIHCR_model(self, y, t, gamma1, gamma2, gamma3, beta, tau, delta):
+        S, I, H, C, R = y
+
+        N = self._N
+
+        dSdt = -(beta * I) * (S / N)
+        dIdt = beta * S * I / N - gamma1 * I - tau * I
+        dHdt = tau * I - gamma2 * H - delta * H
+        dCdt = delta * H - gamma3 * C
+        dRdt = gamma1 * I + gamma2 * H + gamma3 * C
+
+        return [dSdt, dIdt, dHdt, dCdt, dRdt]
+
+    def _plumb(self, params):
+
+        days = len(self._observations)
+
+        # Sarah's function prefers params as a dictionary
+        # so we convert.
+        params_as_dict = self._params_array_to_dict(params)
+
+        res = self._predict(self._initial_conditions, days, params_as_dict)
+
+        rselect = np.ix_(range(res.shape[0]), [1, 2, 3])
+
+        # The genetic algorithm uses an error represented
+        # as a single float => so we can't use a vector.
+        residuals = res[rselect] - self._observations
+        least_squares = np.sum(residuals*residuals)
+        return least_squares
+
+
 if __name__ == "__main__":
     head, observations, rows = load_data()
     rows = np.array(rows)
@@ -352,11 +488,36 @@ if __name__ == "__main__":
 
     res_dict = dict(zip(sres_ndx, range(len(sres_ndx))))
 
-    for t in [ObsRow.POSITIVE, ObsRow.HOSPITALIZED]:
+    plt.figure()
+    for t in [ObsRow.CUMULATIVE_POSITIVE,
+              ObsRow.CUMULATIVE_HOSPITALIZATIONS]:
         plt.plot(sres[:, res_dict[t]], label=f"{t} (model)")
         plt.plot(rows[:, t.value], label=f"{t} (real)")
 
+    plt.title('LM fit')
     plt.xlabel('Days')
     plt.ylabel('Individuals')
+    plt.ylim(0, 1000)
+    plt.legend()
+    plt.show()
+
+    # -------------------------------------------------------------
+
+    ms = Sarah1GA(rows, 1000000)
+    ms.fit_parameters(residuals_error)
+    sres_ndx, sres = ms.predict(30)
+
+    res_dict = dict(zip(sres_ndx, range(len(sres_ndx))))
+
+    plt.figure()
+    for t in [ObsRow.CUMULATIVE_POSITIVE,
+              ObsRow.CUMULATIVE_HOSPITALIZATIONS]:
+        plt.plot(sres[:, res_dict[t]], label=f"{t} (model)")
+        plt.plot(rows[:, t.value], label=f"{t} (real)")
+
+    plt.title('GA fit')
+    plt.xlabel('Days')
+    plt.ylabel('Individuals')
+    plt.ylim(0, 1000)
     plt.legend()
     plt.show()
