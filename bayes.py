d58100b4d212c7c2c87d226e334a989016924e3e
Stéphane Champailler <schampailler@skynet.bewq>
2020-11-23 20:21:32 +0100

    comments, bounds and fitting instead of MAP'ing

--- bayes.py
+++ bayes.py
@@ -1,23 +1,15 @@
 from datetime import datetime
-
-
 from pprint import pprint
 
-import arviz as az
 import numpy as np
+import arviz as az
 import pymc3 as pm
 from pymc3.ode import DifferentialEquation
-
+from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 from utils import ObsEnum, StateEnum, ObsFitEnum, StateFitEnum, Model, residuals_error, load_data, residual_sum_of_squares, log_residual_sum_of_squares
 
-from scipy.integrate import odeint
-
-
-
-
-#TOTAL_POP = 100
 
 def ode_model(ys, t, factor):
 
@@ -43,31 +35,13 @@ def ode_model(ys, t, factor):
     return [dSdt, dEdt, dAdt, dSPdt, dHdt, dCdt, dFdt, dRdt, 0, R_out_HC]
 
 
-    # # Just a toy equation (the suspect equation of SIR)
-
-    # # pymc3 is tricky : pop[0], factor[0] all is important
-
-    # dpop = pop[0] * factor[0] * (TOTAL_POP - pop[0])/TOTAL_POP
-
-    # beta = factor[0]
-
-
-    # dSdt = -beta * S * (A+SP) / N
-    # dEdt = beta * S * (A+SP) / N - rho * E
-
-
-    return dpop
-
-
 
 head, observations, rows = load_data()
 rows = np.array(rows)
 observations = rows.T
 print(observations[ObsEnum.RSURVIVOR.value])
 days = len(observations)
-
-
-STEPS = 50
+STEPS = len(observations[0])
 
 # Integrate differential equations to provide ground truth
 #observations = odeint(ode_model, y0=1, t=range(STEPS), args=([0.2],))
@@ -92,19 +66,21 @@ basic_model = pm.Model()
 with basic_model:
     # See : https://docs.pymc.io/notebooks/getting_started.html
 
-    # b is the parameter governing our model.
-    # that's the one we want to guess.
-    # It's a prior (because it's not based on data).
-    gamma1 = pm.Normal("gamma1", mu=0.02, sigma=0.5)
-    gamma2 = pm.Normal("gamma2", mu=0.74, sigma=0.5)
-    gamma3 = pm.Normal("gamma3", mu=0.88, sigma=0.5)
-    gamma4 = pm.Normal("gamma4", mu=0.07, sigma=0.5)
-    beta = pm.Normal("beta", mu=1, sigma=0.5)
-    tau = pm.Normal("tau", mu=1, sigma=0.5)
-    delta = pm.Normal("delta", mu=0.19, sigma=0.5)
-    sigma = pm.Normal("sigma", mu=0.25, sigma=0.5)
-    rho = pm.Normal("rho", mu=1, sigma=0.5)
-    theta = pm.Normal("theta", mu=0.10, sigma=0.5)
+    # Prior belief (because it's not based on data).
+
+    # Bound = put bounds around the parameter we're modelling
+    # Normal = the parameter is ditributed around some normal distribution
+
+    gamma1 = pm.Bound(pm.Normal, lower=0.02, upper=0.25)("gamma1", mu=0.02, sigma=0.5)
+    gamma2 = pm.Bound(pm.Normal, lower=0.02, upper=1)("gamma2", mu=0.74, sigma=0.5)
+    gamma3 = pm.Bound(pm.Normal, lower=0.02, upper=1)("gamma3", mu=0.88, sigma=0.5)
+    gamma4 = pm.Bound(pm.Normal, lower=0.07, upper=1)("gamma4", mu=0.07, sigma=0.5)
+    beta   = pm.Bound(pm.Normal, lower=0.01, upper=3)("beta", mu=1, sigma=0.5)
+    tau    = pm.Bound(pm.Normal, lower=0.05, upper=1)("tau", mu=1, sigma=0.5)
+    delta  = pm.Bound(pm.Normal, lower=0.09, upper=1)("delta", mu=0.19, sigma=0.5)
+    sigma  = pm.Bound(pm.Normal, lower=0.05, upper=0.25)("sigma", mu=0.25, sigma=0.5)
+    rho    = pm.Bound(pm.Normal, lower=0.20, upper=1)("rho", mu=1, sigma=0.5)
+    theta  = pm.Bound(pm.Normal, lower=0.10, upper=1)("theta", mu=0.10, sigma=0.5)
 
     # We "connect" b to the ODE model
     ode_solution = pm_ode_model(
@@ -125,7 +101,7 @@ with basic_model:
 
     # (Probabilty of having d as a prediction by h)
 
-    #     S, E, A, SP, H, C, F, R
+    # Using the ".T" was seen here : https://docs.pymc.io/notebooks/ODE_API_shapes_and_benchmarking.html
 
     result1 = pm.Normal(
         'Hospi',
@@ -145,8 +121,10 @@ with basic_model:
 
 # Compute "posteriors"
 print(datetime.now())
-map_estimate = pm.find_MAP(model=basic_model)
-pprint(map_estimate)
+approx = pm.fit(model=basic_model)
+
+#map_estimate = pm.find_MAP(model=basic_model)
+#pprint(map_estimate)
 print(datetime.now())
 
 exit()
