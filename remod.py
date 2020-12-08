2ce83f5f081a0fa57541ed317db5958fd76899ef
Stéphane Champailler <schampailler@skynet.bewq>
2020-12-08 16:12:46 +0100

    updates

--- remod.py
+++ remod.py
@@ -1,25 +1,53 @@
-
-import numpy as np
-import math
-from lmfit import minimize, Parameters, report_fit
-#from geneticalgorithm import geneticalgorithm as ga
-from scipy.optimize import minimize as scipy_minimize
-from scipy.stats import binom
-from scipy.special import binom as binomial_coeff
 import random
 import statistics as st
+import numpy as np
+import math
+
+from lmfit import Parameters
+#from geneticalgorithm import geneticalgorithm as ga
+from scipy.optimize import minimize as scipy_minimize
+from scipy.interpolate import interp1d
 from sklearn.neighbors import KernelDensity
 
+from utils import ObsEnum, StateEnum, ObsFitEnum, StateFitEnum, Model, residuals_error, load_data, residual_sum_of_squares, log_residual_sum_of_squares, COLORS_DICT
+
 import matplotlib.pyplot as plt
-from utils import ObsEnum, StateEnum, ObsFitEnum, StateFitEnum, Model, residuals_error, load_data, residual_sum_of_squares, log_residual_sum_of_squares
+import matplotlib.colors as colors
+import matplotlib.cm as cm
+
+
 
 # For repeatiblity (but less random numbers)
 
-# import random
-# random.seed(1000)
-# np.random.seed(1000)
+import random
+random.seed(1000)
+np.random.seed(1000)
 
 SKIP_OBS=10
+
+def population_leave(param, population):
+    # param : the proportion of population that should
+    # leave on average
+
+    if population < 0:
+        # Fix for some edge cases
+        return 0
+
+    # Part of the population that leaves on average
+    average = param * population
+
+    # Binomial centered on the population part
+
+    # The rounding is important because binomial
+    # is for integer number. By using a round we favor
+    # sometimes the high limit sometimes the loaw
+    # limit => on average we center. I think np
+    # will use "int" instead which always favour
+    # the low limit => the distribution is skewed.
+    r = np.random.binomial( round(2*average), 0.5)
+
+    return r
+
 
 class SarahStat(Model):
     def __init__(self, observations, N):
@@ -35,6 +63,7 @@ class SarahStat(Model):
 
         self._preprocess = True
         self._evaluation = 0
+        self._iterations = 0
 
         E0 = 15
         A0 = 10
@@ -187,6 +216,8 @@ class SarahStat(Model):
 
         days = len(self._observations)
 
+        print(params)
+
         # Sarah's function prefers params as a dictionary
         # so we convert.
         params_as_dict = self._params_array_to_dict(params)
@@ -255,7 +286,14 @@ class SarahStat(Model):
         res = self._predict(self._initial_conditions, days, self._fit_params)
         return res
 
-    def _predict(self, initial_conditions, days, params):
+    def predict_stochastic(self, days):
+        res = self._predict(self._initial_conditions, days, self._fit_params,stochastic = True)
+        return res
+
+    def _predict_stochastic(self, initial_conditions, days, params):
+        return self._predict(initial_conditions, days, params, stochastic = True)
+
+    def _predict(self, initial_conditions, days, params, stochastic = False):
         gamma1 = params['gamma1']
         gamma2 = params['gamma2']
         gamma3 = params['gamma3']
@@ -274,7 +312,11 @@ class SarahStat(Model):
 
         for d in range(days):
             ys = [S, E, A, SP, H, C, F, R]
-            dSdt, dEdt, dAdt, dSPdt, dHdt, dCdt, dFdt, dRdt, dHOutdt = self._model(ys, gamma1, gamma2, gamma3, gamma4, beta, tau, delta, sigma, rho, theta)
+
+            if stochastic:
+                dSdt, dEdt, dAdt, dSPdt, dHdt, dCdt, dFdt, dRdt, dHOutdt = self._model_stochastic(ys, gamma1, gamma2, gamma3, gamma4, beta, tau, delta, sigma, rho, theta)
+            else:
+                dSdt, dEdt, dAdt, dSPdt, dHdt, dCdt, dFdt, dRdt, dHOutdt = self._model(ys, gamma1, gamma2, gamma3, gamma4, beta, tau, delta, sigma, rho, theta)
 
             S += dSdt
             E += dEdt
@@ -316,10 +358,155 @@ class SarahStat(Model):
         return [dSdt, dEdt, dAdt, dSPdt, dHdt, dCdt, dFdt, dRdt, dHOutdt]
 
 
+
+    def _model_stochastic(self, ys, gamma1, gamma2, gamma3, gamma4, beta, tau, delta, sigma, rho, theta):
+        S, E, A, SP, H, C, F, R = ys
+
+        N = self._N
+
+        # betaS = beta * S * (A+SP) / N
+        # rhoE = rho * E
+        # sigmaA = sigma * A
+        # gamma4A = gamma4 * A
+        # tauSP = tau * SP
+        # gamma1SP = gamma1 * SP
+        # deltaH = delta * H
+        # gamma2H = gamma2 * H
+        # thetaC = theta * C
+        # gamma3C = gamma3 * C
+
+        betaS = population_leave(beta, S * (A+SP) / N)
+        rhoE = population_leave(rho, E)
+        sigmaA = population_leave(sigma, A)
+        gamma4A = population_leave(gamma4, A)
+        tauSP = population_leave(tau, SP)
+        gamma1SP = population_leave(gamma1, SP)
+        deltaH = population_leave(delta, H)
+        gamma2H = population_leave(gamma2, H)
+        thetaC = population_leave(theta, C)
+        gamma3C = population_leave(gamma3, C)
+
+        dSdt = -betaS
+        dEdt =  betaS - rhoE
+        #dAdt = rho * E - sigma*E - gamma4 * A
+        dAdt = rhoE - sigmaA - gamma4A
+        #dSPdt = sigma * E - tau * SP - gamma1 * SP
+        dSPdt = sigmaA - tauSP - gamma1SP
+        dHdt = tauSP - deltaH - gamma2H
+        dCdt = deltaH - thetaC - gamma3C
+        dFdt = thetaC
+        dRdt = gamma1SP + gamma2H + gamma3C + gamma4A
+
+        dHOutdt = tau*SP
+
+        return [dSdt, dEdt, dAdt, dSPdt, dHdt, dCdt, dFdt, dRdt, dHOutdt]
+
+
     def _params_array_to_dict(self, params):
         return dict(
             zip(['gamma1', 'gamma2', 'gamma3', 'gamma4', 'beta', 'tau', 'delta', 'sigma','rho','theta'],
                 params))
+
+
+    def compute_log_likelihoods(self, all_exp, obs_rows):
+        lhs = dict()
+
+        days = obs_rows.shape[1]
+
+        for state, obs in [(StateEnum.HOSPITALIZED, ObsEnum.NUM_HOSPITALIZED),
+                           (StateEnum.CRITICAL, ObsEnum.NUM_CRITICAL),
+                           (StateEnum.FATALITIES, ObsEnum.NUM_FATALITIES)]:
+
+            log_likelihood = 0
+            for day_ndx in range(days):
+                # Take all the values of experiments on a given day day_ndx
+                # for a given measurement (state.value)
+                d = all_exp[:, day_ndx, state.value]
+
+                # Compute the PDF of the epxeriments
+                model = KernelDensity(bandwidth=max(1, (np.amax(d) / 10)), kernel='gaussian')
+                model.fit(d.reshape(-1, 1))
+
+                observation = obs_rows[day_ndx, obs.value]
+
+                # score sample return log of probabilities !
+                # score_samples returns log of probability
+                # exp(...) => Probaiblity
+                log_likelihood += model.score_samples([[observation]])
+
+            lhs[obs] = log_likelihood
+
+            #print(f"likelihood {state} over {days} days: log_likelihood:{log_likelihood}")
+
+        return lhs
+
+
+    def _plumb_scipy_stocha(self, params):
+
+        days = len(self._observations)
+
+        # Sarah's function prefers params as a dictionary
+        # so we convert.
+        params_as_dict = self._params_array_to_dict(params)
+
+        NB_EXPERIMENTS = 1000
+        PREDICTED_DAYS = 80
+
+        # print(f"Running {NB_EXPERIMENTS} experiments")
+
+        experiments = [] # dims : [experiment #][day][value]
+
+        for i in range(NB_EXPERIMENTS):
+            sres = ms._predict_stochastic(
+                self._initial_conditions, PREDICTED_DAYS,
+                params_as_dict)
+            experiments.append(sres)
+        # print("... done running experiments")
+
+        all_exp = np.stack(experiments)
+        log_likelihoods = self.compute_log_likelihoods(all_exp, rows)
+
+        # FIXME Check the sign
+        ll = - sum(log_likelihoods.values())
+        print(f"{self._iterations} {ll}")
+        print(params)
+        self._iterations += 1
+
+        return ll
+
+
+    def stocha_fit_parameters(self):
+        # L-BFGS-B accepts bounds
+
+        np.seterr(all='raise')
+
+        # Find first set of parameters
+        params = self.get_initial_parameters()
+        self.fit_parameters(residual_sum_of_squares)
+
+        # Group parameters
+        fit_params = self._fit_params
+        bounds = []
+        x0 = []
+        for k in fit_params.keys():
+            p = params[k]
+            bounds.append((p.min, p.max))
+            x0.append(fit_params[k])
+
+
+        res = scipy_minimize(self._plumb_scipy_stocha,
+                             x0=x0,
+                             method='L-BFGS-B',
+                             bounds=bounds)
+
+        print(res)
+
+        self._fit_params = self._params_array_to_dict(res.x)
+
+        for p_name, p in params.items():
+            print( "{:10s} [{:.2f} - {:.2f}] : {:.2f}".format(p_name,p.min, p.max,self._fit_params[p_name]))
+
+
 
 if __name__ == "__main__":
     head, observations, rows = load_data()
@@ -327,16 +514,111 @@ if __name__ == "__main__":
     days = len(observations)
 
     ms = SarahStat(rows, 1000000)
+
+    ms.stocha_fit_parameters()
+
     ms.fit_parameters(residual_sum_of_squares)
-    sres = ms.predict(250)
 
     plt.figure()
     plt.title('LM fit')
+
+
+    NB_EXPERIMENTS = 1000
+    PREDICTED_DAYS = 80
+
+    print(f"Running {NB_EXPERIMENTS} experiments")
+    experiments = [] # dims : [experiment #][day][value]
+
+    for i in range(NB_EXPERIMENTS):
+        sres = ms.predict_stochastic(PREDICTED_DAYS)
+        experiments.append(sres)
+    print("... done running experiments")
+
+    all_exp = np.stack(experiments)
+    log_likelihoods = compute_log_likelihoods(all_exp, rows)
+    log_likelihoods_all_params = sum(log_likelihoods.values())
+
+    exit()
+
+    NB_BINS = NB_EXPERIMENTS//20
+
+    for state, obs in [(StateEnum.HOSPITALIZED, ObsEnum.NUM_HOSPITALIZED),
+                       (StateEnum.CRITICAL, ObsEnum.NUM_CRITICAL)]:
+
+        likelihood = 1
+        log_likelihood = 0
+        for day_ndx in range(days):
+            d = all_exp[:, day_ndx, state.value]
+            histo = np.histogram(d,bins=NB_BINS)
+
+            # From histogram counts to probabilities
+            proba = histo[0] * (1/np.sum(histo[0]))
+
+            # In which bin fits the observation ?
+            observation = rows[day_ndx, obs.value]
+            tbin = np.digitize(observation, histo[1]) - 1
+
+            if tbin < 0:
+                tbin = 0
+            elif tbin >= NB_BINS:
+                tbin = NB_BINS - 1
+
+
+            prob = proba[tbin]
+
+            if True or prob == 0:
+                # print(histo)
+                # print(observation)
+
+                # x = np.arange(len(proba))
+                # idx = np.nonzero(proba)
+                # interp = interp1d(x[idx], proba[idx])
+                # print(interp(tbin))
+
+                model = KernelDensity(bandwidth=max(1, (np.amax(d) / 10)), kernel='gaussian')
+                model.fit(d.reshape(-1, 1))
+
+
+                ls = np.linspace(observation-0.5,observation+0.5,2)[:,np.newaxis]
+                prob = np.sum( np.exp( model.score_samples(ls))) / 2
+
+                prob2 = model.score_samples([[observation]])
+
+
+
+                # # Debug
+                # print(f"obs:{observation} prob:{prob} prob2:{prob2}")
+                # plt.figure()
+
+                # density, bins = np.histogram(d, bins=NB_BINS, density=True)
+                # unity_density = density / density.sum()
+                # widths = bins[:-1] - bins[1:]
+                # #plt.bar(bins[1:], unity_density, width=widths)
+
+                # plt.hist(d,bins=NB_BINS,density=True)
+
+                # log_dens = model.score_samples(histo[1].reshape( -1, 1))
+                # plt.plot(histo[1], np.exp(log_dens), c='red')
+                # plt.title(f"Day:{day_ndx}")
+                # plt.show()
+
+            likelihood *= prob
+            log_likelihood += prob2
+
+        print(f"likelihood {state}: {likelihood}, log_likelihood:{log_likelihood}")
+
+    exit()
+
+    for sres in experiments:
+        for t in [StateEnum.RSURVIVOR]: #, StateEnum.HOSPITALIZED, StateEnum.CRITICAL, StateEnum.FATALITIES]:
+            plt.plot(sres[:, t.value], c=COLORS_DICT[t], alpha=0.05, linewidth=0.4)
+
+    sres = ms.predict(250)
     for t in [StateEnum.RSURVIVOR, StateEnum.HOSPITALIZED, StateEnum.CRITICAL, StateEnum.FATALITIES]:
-        plt.plot(sres[:, t.value], label=f"{t} (model)")
+        plt.plot(sres[:, t.value], c=COLORS_DICT[t], label=f"{t} (model)")
 
     for u in [ObsEnum.RSURVIVOR, ObsEnum.NUM_HOSPITALIZED, ObsEnum.NUM_CRITICAL,ObsEnum.NUM_FATALITIES]:
-        plt.plot(rows[:, u.value], label=f"{u} (real)")
+        plt.plot(rows[:, u.value], "--", c=COLORS_DICT[u], label=f"{u} (real)")
 
 
     plt.title('Curve Fitting')
@@ -349,6 +631,19 @@ if __name__ == "__main__":
     plt.savefig('data_fit.pdf')
     plt.savefig(f'data_fit_{days}_days.pdf')
     plt.show()
+
+    plt.figure()
+    jet = plt.get_cmap('jet')
+    days_max = min(200, PREDICTED_DAYS)
+    cNorm  = colors.Normalize(vmin=20, vmax=days_max)
+    scalarMap = cm.ScalarMappable(norm=cNorm, cmap=jet)
+    for day in range(60,days_max):
+        d = [experiments[exp_i][day, StateEnum.HOSPITALIZED.value] for exp_i in range(NB_EXPERIMENTS)]
+        hd = np.histogram(d,bins=50)[0]
+        plt.plot(hd, c=scalarMap.to_rgba(day), alpha=0.2)
+    plt.title("Normalized histograms of 1000/day hospitalized values (color = day)")
+    plt.xticks([])
+    plt.colorbar(scalarMap)
 
     plt.figure()
     for t in [StateEnum.EXPOSED, StateEnum.ASYMPTOMATIQUE, StateEnum.SYMPTOMATIQUE ,StateEnum.HOSPITALIZED, StateEnum.CRITICAL, StateEnum.FATALITIES]:
