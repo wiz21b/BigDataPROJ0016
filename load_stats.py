3217d829b3cae7ce526422e9da2ec85f1e857667
Stéphane Champailler <schampailler@skynet.bewq>
2020-12-12 15:20:33 +0100

    more text

--- load_stats.py
+++ load_stats.py
@@ -2,7 +2,7 @@ import urllib.request
 import json
 
 with urllib.request.urlopen("https://raw.githubusercontent.com/ADelau/proj0016-epidemic-data/main/stats/age.json") as fp:
-    split = [ l.decode("utf-8").strip().split(",") for l in fp.readlines()]
+    split = [l.decode("utf-8").strip().split(",") for l in fp.readlines()]
 
     data = [(int(age), int(nb)) for age, nb in split]
 
@@ -42,17 +42,60 @@ STATS_SCHOOLS = parse_places("https://raw.githubusercontent.com/ADelau/proj0016-
 STATS_WORKPLACES = parse_places("https://raw.githubusercontent.com/ADelau/proj0016-epidemic-data/main/stats/workplaces.json")
 
 
-print(STATS_AGES[10:15])
-print(f"Population = {sum(STATS_AGES)} persons")
+if __name__ == "__main__":
 
-print(STATS_COMMUNITIES_POP[10:15])
-print(f"Population in communities = {sum(STATS_COMMUNITIES_POP[1:])} persons")
-print(STATS_COMMUNITIES_FRAC[10:15])
+    # STATS_AGES[ age ] = nb de personnes avec cet âge.
+    print(STATS_AGES[10:15])
+    print(f"Population = {sum(STATS_AGES)} persons")
 
-print(STATS_HOUSEHOLDS[1:4])
-s = sum([i*STATS_HOUSEHOLDS[i] for i in range(len(STATS_HOUSEHOLDS))])
-print(f"Population in households = {s} persons")
+    # STATS_COMMUNITIES_POP[i] = nbre de personne dans communauté i
+    print(STATS_COMMUNITIES_POP[10:15])
+    print(f"Population in communities = {sum(STATS_COMMUNITIES_POP[1:])} persons")
+    print(STATS_COMMUNITIES_FRAC[10:15])
 
+    # STATS_HOUSEHOLDS[i] = nbre de ménage de taille i
+    print(STATS_HOUSEHOLDS[1:4])
+    s = sum([i*STATS_HOUSEHOLDS[i] for i in range(len(STATS_HOUSEHOLDS))])
+    print(f"Population in households = {s} persons")
 
-print(STATS_SCHOOLS[10:15])
-print(STATS_WORKPLACES[10:15])
+    # STATS_SCHOOLS[i] = (bounds, nb) : nbre nb d'écoles dont le nbre d'élèves est ntre bound[0] et bound[1]
+
+    print(STATS_SCHOOLS[10:15])
+    s = 0
+    for bounds, nb_schools in STATS_SCHOOLS:
+        nb_students = (bounds[0] + bounds[1]) / 2
+        s += nb_students * nb_schools
+    print(f"Total students in schools = {s}")
+
+    # WORKPLACES[i] = (bounds, nb) : nbre nb d'écoles dont le nbre d'élèves est ntre bound[0] et bound[1]
+    print(STATS_WORKPLACES[10:15])
+    s = 0
+    for bounds, nb_workplaces in STATS_WORKPLACES:
+        nb_workers = (bounds[0] + bounds[1]) / 2
+        s += nb_workers * nb_workplaces
+    print(f"Total workers at work = {s}")
+
+    """
+    # Case Isolation ---------------------------------------
+
+    Isolated person infection rate is
+    - 0.25 times less when at home
+    - 0.90 times less when in communities
+    - 1 times less when at work (so basically he doesn't go to school/work anymore)
+
+    It has also a duration of isolation
+
+    Assumptions :
+    ------------
+
+    This affects our "exposed" compartment.
+
+        dEdt = beta * S * (A+SP) / N - rho * E
+
+    When someone is in E and isolation comes in, rho is affected.
+
+    I assume :
+    - All workers are at home 16hours a day (and 8 hours a day at work)
+    - Non workers are at home 16hours a day
+
+    """
