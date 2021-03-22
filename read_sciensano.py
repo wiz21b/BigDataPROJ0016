d030513b10509bc2ed8f0ca84f03b2e66902c950
Stéphane Champailler <schampailler@skynet.bewq>
2021-03-22 21:48:53 +0100

    clean ups

--- read_sciensano.py
+++ read_sciensano.py
@@ -5,8 +5,22 @@ import urllib.request
 from io import StringIO
 import pandas
 
+_URLS = [("https://epistat.sciensano.be/Data/COVID19BE_CASES_MUNI_CUM.csv"),
+         ("https://epistat.sciensano.be/Data/COVID19BE_CASES_AGESEX.csv"),
+         ("https://epistat.sciensano.be/Data/COVID19BE_CASES_MUNI.csv"),
+         ("https://epistat.sciensano.be/Data/COVID19BE_HOSP.csv"),
+         ("https://epistat.sciensano.be/Data/COVID19BE_MORT.csv"),
+         ("https://epistat.sciensano.be/Data/COVID19BE_tests.csv"),
+         ("https://epistat.sciensano.be/Data/COVID19BE_VACC.csv")]
 
-def read_csv(url):
+
+def read_csv(url) -> pandas.DataFrame:
+    """ Read a CSV file at url.
+
+    Will cache a copy so that subsequent runs will reuse the
+    copy, avoid HTTP connection and improving load times by
+    a factor of 10. The copy is refreshed every day.
+    """
 
     name = url.rsplit('/', 1)[-1]
     date = datetime.date.today().strftime("%Y%m%d")
@@ -15,11 +29,11 @@ def read_csv(url):
     fpath = os.path.join(tempfile.gettempdir(), fname)
 
     if os.path.exists(fpath):
-        print(f"Reading cached file {fpath}")
+        # print(f"Reading cached file {fpath}")
         with open(fpath, "rb") as fp:
             data = fp.read()
     else:
-        print(f"Loading data from Sciensano {url}")
+        # print(f"Loading data from Sciensano {url}")
         with urllib.request.urlopen(url) as fp:
             data = fp.read()
 
@@ -32,35 +46,22 @@ def read_csv(url):
     else:
         parse_dates = False
 
-    dtypes= {}
+    dtypes = {}
     if "NIS5" in data:
         dtypes['NIS5'] = pandas.Int64Dtype()
 
-    csv = pandas.read_csv( StringIO(data),
-                           dtype = dtypes,
-                           parse_dates=parse_dates)
+    csv = pandas.read_csv(StringIO(data),
+                          dtype=dtypes,
+                          parse_dates=parse_dates)
 
     # csv.info()
     # print(csv)
     return csv
 
 
-_URLS = [("https://epistat.sciensano.be/Data/COVID19BE_CASES_MUNI_CUM.csv"),
-         ("https://epistat.sciensano.be/Data/COVID19BE_CASES_AGESEX.csv"),
-         ("https://epistat.sciensano.be/Data/COVID19BE_CASES_MUNI.csv"),
-         ("https://epistat.sciensano.be/Data/COVID19BE_HOSP.csv"),
-         ("https://epistat.sciensano.be/Data/COVID19BE_MORT.csv"),
-         ("https://epistat.sciensano.be/Data/COVID19BE_tests.csv"),
-         ("https://epistat.sciensano.be/Data/COVID19BE_VACC.csv")]
-
 start_time = datetime.datetime.now()
-
-
-csvs = [read_csv(url) for url in _URLS]
-CASES_MUNI_CUM, CASES_AGESEX, CASES_MUNI, HOSP, MORT, TESTS, VACC = csvs
-
-
+_csvs = [read_csv(url) for url in _URLS]
 end_time = datetime.datetime.now()
-delta = end_time - start_time
-duration = delta.seconds + delta.microseconds/1000000
-print(f"Loaded data in {duration:.2f} sec.")
+print(f"Loaded data in {(end_time - start_time).total_seconds():.2f} sec.")
+
+CASES_MUNI_CUM, CASES_AGESEX, CASES_MUNI, HOSP, MORT, TESTS, VACC = _csvs
