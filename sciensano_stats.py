e40948b024d541ae2004120d2aefb550783e0e2c
Stéphane Champailler <schampailler@skynet.bewq>
2021-03-25 21:35:47 +0100

    more stuff

--- sciensano_stats.py
+++ sciensano_stats.py
@@ -21,8 +21,17 @@ plt.plot(cumul_total_deaths['DATE'], cumul_total_deaths['DEATHS'], label="Deaths
 cumul_tests = TESTS[["DATE","TESTS_ALL","TESTS_ALL_POS"]].groupby("DATE").sum().reset_index()
 plt.plot(cumul_tests['DATE'], cumul_tests['TESTS_ALL'].rolling(window=14).mean(), label="Tests/day")
 plt.plot(cumul_tests['DATE'], cumul_tests['TESTS_ALL_POS'].rolling(window=14).mean(), label="Positive/day")
-print(TESTS)
 
+print(VACC)
+
+print(list(VACC.columns.values))
+first_dose = VACC[["DATE","DOSE","COUNT"]].query('DOSE == "A"')
+cumul_vacc_1st_dose = first_dose.groupby("DATE").sum().reset_index()
+second_dose = VACC[["DATE","DOSE","COUNT"]].query('DOSE == "B"')
+cumul_vacc_2nd_dose = second_dose.groupby("DATE").sum().reset_index()
+
+plt.plot(cumul_vacc_1st_dose['DATE'], cumul_vacc_1st_dose['COUNT'].rolling(window=14).mean(), label="1st dose/day")
+plt.plot(cumul_vacc_2nd_dose['DATE'], cumul_vacc_2nd_dose['COUNT'].rolling(window=14).mean(), label="2nd dose/day")
 
 plt.title("Sciensano dataset")
 plt.legend()
