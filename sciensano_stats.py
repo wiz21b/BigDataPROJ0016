1a7d1dea182cd26842204cafa8d50b8bc4eb1dd2
Stéphane Champailler <schampailler@skynet.bewq>
2021-03-28 11:22:27 +0200

    restructured scinsao data reading

--- sciensano_stats.py
+++ sciensano_stats.py
@@ -1,42 +1,47 @@
 import matplotlib.pyplot as plt
-
-from read_sciensano import *
+from utils import load_sciensano_data
 
 def rolling_mean(df, window):
     return df
     #return df.rolling(window=window).mean()
 
-# CASES_MUNI_CUM, CASES_AGESEX, CASES_MUNI, HOSP, MORT, TESTS, VACC = _read_data()
+if __name__ == "__main__":
 
-cumul_cases = CASES_MUNI[["DATE","CASES"]].groupby("DATE").sum().reset_index()
+    """ The goal of this pogram is to show the sciensano
+    statistics as they are. Just to look at them.
+    """
 
-plt.plot(cumul_cases['DATE'], rolling_mean(cumul_cases['CASES'], 14), label="New case/day")
+    CASES_MUNI_CUM, CASES_AGESEX, CASES_MUNI, HOSP, MORT, TESTS, VACC = load_sciensano_data()
 
-cumul_total_in = HOSP[["DATE","TOTAL_IN","TOTAL_IN_ICU"]].groupby("DATE").sum().reset_index()
+    cumul_cases = CASES_MUNI[["DATE","CASES"]].groupby("DATE").sum().reset_index()
 
-plt.plot(cumul_total_in['DATE'], rolling_mean(cumul_total_in['TOTAL_IN'],6), label="In hospital/day")
-plt.plot(cumul_total_in['DATE'], cumul_total_in['TOTAL_IN_ICU'], label="In ICU/day")
-plt.xlabel("Time")
-plt.ylabel("Individuals")
+    plt.plot(cumul_cases['DATE'], rolling_mean(cumul_cases['CASES'], 14), label="New case/day")
 
-cumul_total_deaths = MORT[["DATE","DEATHS"]].groupby("DATE").sum().reset_index()
-plt.plot(cumul_total_deaths['DATE'], cumul_total_deaths['DEATHS'], label="Deaths/day")
+    cumul_total_in = HOSP[["DATE","TOTAL_IN","TOTAL_IN_ICU"]].groupby("DATE").sum().reset_index()
 
-cumul_tests = TESTS[["DATE","TESTS_ALL","TESTS_ALL_POS"]].groupby("DATE").sum().reset_index()
-plt.plot(cumul_tests['DATE'], rolling_mean(cumul_tests['TESTS_ALL'],14), label="Tests/day")
-plt.plot(cumul_tests['DATE'], rolling_mean(cumul_tests['TESTS_ALL_POS'],14), label="Positive/day")
+    plt.plot(cumul_total_in['DATE'], rolling_mean(cumul_total_in['TOTAL_IN'],6), label="In hospital/day")
+    plt.plot(cumul_total_in['DATE'], cumul_total_in['TOTAL_IN_ICU'], label="In ICU/day")
+    plt.xlabel("Time")
+    plt.ylabel("Individuals")
 
-print(VACC)
+    cumul_total_deaths = MORT[["DATE","DEATHS"]].groupby("DATE").sum().reset_index()
+    plt.plot(cumul_total_deaths['DATE'], cumul_total_deaths['DEATHS'], label="Deaths/day")
 
-print(list(VACC.columns.values))
-first_dose = VACC[["DATE","DOSE","COUNT"]].query('DOSE == "A"')
-cumul_vacc_1st_dose = first_dose.groupby("DATE").sum().reset_index()
-second_dose = VACC[["DATE","DOSE","COUNT"]].query('DOSE == "B"')
-cumul_vacc_2nd_dose = second_dose.groupby("DATE").sum().reset_index()
+    cumul_tests = TESTS[["DATE","TESTS_ALL","TESTS_ALL_POS"]].groupby("DATE").sum().reset_index()
+    plt.plot(cumul_tests['DATE'], rolling_mean(cumul_tests['TESTS_ALL'],14), label="Tests/day")
+    plt.plot(cumul_tests['DATE'], rolling_mean(cumul_tests['TESTS_ALL_POS'],14), label="Positive/day")
 
-plt.plot(cumul_vacc_1st_dose['DATE'], rolling_mean(cumul_vacc_1st_dose['COUNT'],14), label="1st dose/day")
-plt.plot(cumul_vacc_2nd_dose['DATE'], rolling_mean(cumul_vacc_2nd_dose['COUNT'],14), label="2nd dose/day")
+    print(VACC)
 
-plt.title("Sciensano dataset")
-plt.legend()
-plt.show()
+    print(list(VACC.columns.values))
+    first_dose = VACC[["DATE","DOSE","COUNT"]].query('DOSE == "A"')
+    cumul_vacc_1st_dose = first_dose.groupby("DATE").sum().reset_index()
+    second_dose = VACC[["DATE","DOSE","COUNT"]].query('DOSE == "B"')
+    cumul_vacc_2nd_dose = second_dose.groupby("DATE").sum().reset_index()
+
+    plt.plot(cumul_vacc_1st_dose['DATE'], rolling_mean(cumul_vacc_1st_dose['COUNT'],14), label="1st dose/day")
+    plt.plot(cumul_vacc_2nd_dose['DATE'], rolling_mean(cumul_vacc_2nd_dose['COUNT'],14), label="2nd dose/day")
+
+    plt.title("Sciensano dataset")
+    plt.legend()
+    plt.show()
--- utils.py
+++ utils.py
@@ -5,10 +5,16 @@ import csv
 from collections import namedtuple
 import urllib.request
 
+import os
+import datetime
+import tempfile
+from io import StringIO
+import pandas
+
+
 COLORS = ['red', 'green', 'blue', 'magenta', 'purple',
           'lime', 'orange', 'chocolate', 'gray',
           'darkgreen', 'darkviolet']
-
 
 
 class ObsEnum(Enum):
@@ -47,7 +53,6 @@ class ObsEnum(Enum):
     # Other data not in the dataset
     # RECOVERED = 10
     # SUSPECT = 12
-
 
     def __int__(self):
         return self.value
@@ -264,3 +269,73 @@ def load_data( url="https://raw.githubusercontent.com/ADelau/proj0016-epidemic-d
                 rows.append(ir + [positive_cumulated, tested_cumulated,rsurvivor,dhdt,dfdt])
 
     return head, observations, rows
+
+
+_SCIENSANO_URLS = [
+    ("https://epistat.sciensano.be/Data/COVID19BE_CASES_MUNI_CUM.csv"),
+    ("https://epistat.sciensano.be/Data/COVID19BE_CASES_AGESEX.csv"),
+    ("https://epistat.sciensano.be/Data/COVID19BE_CASES_MUNI.csv"),
+    ("https://epistat.sciensano.be/Data/COVID19BE_HOSP.csv"),
+    ("https://epistat.sciensano.be/Data/COVID19BE_MORT.csv"),
+    ("https://epistat.sciensano.be/Data/COVID19BE_tests.csv"),
+    ("https://epistat.sciensano.be/Data/COVID19BE_VACC.csv")]
+
+
+def _read_csv(url) -> pandas.DataFrame:
+    """ Read a CSV file at url.
+
+    Will cache a copy so that subsequent runs will reuse the
+    copy, avoid HTTP connection and improving load times by
+    a factor of 10. The copy is refreshed every day.
+    """
+
+    name = url.rsplit('/', 1)[-1]
+    date = datetime.date.today().strftime("%Y%m%d")
+
+    fname = f"cache_{date}_{name}"
+    fpath = os.path.join(tempfile.gettempdir(), fname)
+
+    if os.path.exists(fpath):
+        # print(f"Reading cached file {fpath}")
+        with open(fpath, "rb") as fp:
+            data = fp.read()
+    else:
+        # print(f"Loading data from Sciensano {url}")
+        with urllib.request.urlopen(url) as fp:
+            data = fp.read()
+
+        with open(fpath, "wb") as fp:
+            fp.write(data)
+
+    data = data.decode("utf-8")
+    if "DATE" in data:
+        parse_dates = ['DATE']
+    else:
+        parse_dates = False
+
+    dtypes = {}
+    if "NIS5" in data:
+        dtypes['NIS5'] = pandas.Int64Dtype()
+
+    csv = pandas.read_csv(StringIO(data),
+                          dtype=dtypes,
+                          parse_dates=parse_dates)
+
+    # csv.info()
+    # print(csv)
+    return csv
+
+
+def load_sciensano_data():
+    start_time = datetime.datetime.now()
+    _csvs = [_read_csv(url) for url in _SCIENSANO_URLS]
+    end_time = datetime.datetime.now()
+    print(f"Loaded data in {(end_time - start_time).total_seconds():.2f} sec.")
+
+    CASES_MUNI_CUM, CASES_AGESEX, CASES_MUNI, HOSP, MORT, TESTS, VACC = _csvs
+    # Fixing data entries equal to "<5"
+    CASES_MUNI["CASES"].replace("<5", "2.5", inplace=True)
+    # Fixing type
+    CASES_MUNI["CASES"] = pandas.to_numeric(CASES_MUNI["CASES"])
+
+    return CASES_MUNI_CUM, CASES_AGESEX, CASES_MUNI, HOSP, MORT, TESTS, VACC
